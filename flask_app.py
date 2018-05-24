4aacd1bf3f1be144bd1311d2b00ec8857af10f11
Belloz <krecik704@gmail.com>
2018-05-23 23:13:00 -0400

    First Init

---
+++ flask_app.py
@@ -0,0 +1,16 @@
+from flask import Flask, request, redirect, render_template
+from sound_cloud_downloader import get_link
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+	return render_template('my-form.html')
+	#return "Hello World! So simple!"
+	
+@app.route("/download_mp3", methods=['GET'])	
+def download_mp3():
+	#media_url = request.args.get('url', '')
+	media_url = request.args['url'] #request.form[...] didnt work so alternatively .args[...]
+	return redirect(get_link(media_url), code=302)
+	
---
+++ sound_cloud_downloader.py
@@ -0,0 +1,11 @@
+import re
+import requests
+import sys
+
+def get_link(url):
+	html = requests.get(url)
+	regEx = re.compile(r'soundcloud://sounds:(.+?)"')
+	mo = re.search(regEx, html.text)
+	final_page = requests.get("https://api.soundcloud.com/i1/tracks/{0}/streams?client_id=eeBnt1uSZxQBwR3x2gyLtF5nekoouMvM".format(mo.group(1)))
+
+	return final_page.json()['http_mp3_128_url']
